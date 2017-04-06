327dd2c321bf457d17a6ec8f7c27d381796d3845
Brandon Jones <brandon.jones@blackbaud.com>
2017-04-06 14:46:45 -0400

    added rating (#7)

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -12,7 +12,10 @@ export class HomeComponent {
         name: 'Nintendo inc.'
       },
       description: 'Bacon ipsum dolor amet pork loin boudin brisket ham hock, tri-tip strip steak tenderloin prosciutto pancetta frankfurter pork. Strip steak tenderloin pork hamburger capicola bresaola jerky short loin beef ribs tail frankfurter pork belly burgdoggen. Pork loin venison fatback hamburger ball tip t-bone filet mignon pig flank sirloin. Turducken pork chop ribeye salami.',
-      logo_url: 'http://europaplus.com.ua/wp-content/photos/2014/09/Nintendo-symbol-228x131.jpg'
+      logo_url: 'http://europaplus.com.ua/wp-content/photos/2014/09/Nintendo-symbol-228x131.jpg',
+      rating: {
+        value: 4
+      }
     },
     {
       name: 'Aliens',
@@ -20,7 +23,10 @@ export class HomeComponent {
         name: 'Android'
       },
       description: 'Bacon ipsum dolor amet pork loin boudin brisket ham hock, tri-tip strip steak tenderloin prosciutto pancetta frankfurter pork. Strip steak tenderloin pork hamburger capicola bresaola jerky short loin beef ribs tail frankfurter pork belly burgdoggen. Pork loin venison fatback hamburger ball tip t-bone filet mignon pig flank sirloin. Turducken pork chop ribeye salami.',
-      logo_url: 'http://movilarena.com/wp-content/uploads/2010/12/android-logo-310x350.png'
+      logo_url: 'http://movilarena.com/wp-content/uploads/2010/12/android-logo-310x350.png',
+      rating: {
+        value: 2
+      }
     },
     {
       name: 'Coffee Drinks',
@@ -28,7 +34,10 @@ export class HomeComponent {
         name: 'Starbucks'
       },
       description: 'Bacon ipsum dolor amet pork loin boudin brisket ham hock, tri-tip strip steak tenderloin prosciutto pancetta frankfurter pork. Strip steak tenderloin pork hamburger capicola bresaola jerky short loin beef ribs tail frankfurter pork belly burgdoggen. Pork loin venison fatback hamburger ball tip t-bone filet mignon pig flank sirloin. Turducken pork chop ribeye salami. Bacon ipsum dolor amet pork loin boudin brisket ham hock, tri-tip strip steak tenderloin prosciutto pancetta frankfurter pork. Strip steak tenderloin pork hamburger capicola bresaola jerky short loin beef ribs tail frankfurter pork belly burgdoggen. Pork loin venison fatback hamburger ball tip t-bone filet mignon pig flank sirloin. Turducken pork chop ribeye salami.',
-      logo_url: 'http://pitweston.com/wp-content/uploads/2009/12/starbucks-logo-150x150.jpg'
+      logo_url: 'http://pitweston.com/wp-content/uploads/2009/12/starbucks-logo-150x150.jpg',
+      rating: {
+        value: 2.84
+      }
     },
     {
       name: 'Flower Power',
@@ -36,7 +45,10 @@ export class HomeComponent {
         name: 'Nintendo inc.'
       },
       description: 'Bacon ipsum dolor amet pork loin boudin brisket ham hock, tri-tip strip steak tenderloin prosciutto pancetta frankfurter pork. Strip steak tenderloin pork hamburger capicola bresaola jerky short loin beef ribs tail frankfurter pork belly burgdoggen. Pork loin venison fatback hamburger ball tip t-bone filet mignon pig flank sirloin. Turducken pork chop ribeye salami.',
-      logo_url: 'http://europaplus.com.ua/wp-content/photos/2014/09/Nintendo-symbol-228x131.jpg'
+      logo_url: 'http://europaplus.com.ua/wp-content/photos/2014/09/Nintendo-symbol-228x131.jpg',
+      rating: {
+        value: 2.84
+      }
     },
     {
       name: 'Flower Power',
@@ -44,7 +56,10 @@ export class HomeComponent {
         name: 'Nintendo inc.'
       },
       description: 'Bacon ipsum dolor amet pork loin boudin brisket ham hock, tri-tip strip steak tenderloin prosciutto pancetta frankfurter pork. Strip steak tenderloin pork hamburger capicola bresaola jerky short loin beef ribs tail frankfurter pork belly burgdoggen. Pork loin venison fatback hamburger ball tip t-bone filet mignon pig flank sirloin. Turducken pork chop ribeye salami.',
-      logo_url: 'http://europaplus.com.ua/wp-content/photos/2014/09/Nintendo-symbol-228x131.jpg'
+      logo_url: 'http://europaplus.com.ua/wp-content/photos/2014/09/Nintendo-symbol-228x131.jpg',
+      rating: {
+        value: 1
+      }
     }
-  ]
+  ];
 }
