df7b4ac76d006e427c71f6e69eab7d39773a8cb6
Steve Brush <steve.brush@blackbaud.com>
2017-04-06 12:31:33 -0400

    Updated dependencies (#1)

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 
+import { StacheModule } from '@blackbaud/stache';
+
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
+  imports: [StacheModule],
+  exports: [StacheModule],
   providers: [],
   entryComponents: []
 })
