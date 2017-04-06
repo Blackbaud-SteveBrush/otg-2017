4b971e48ad62a97463a90ef2761283961d32f3cb
Brandon Hare <blackbaud-brandonhare@users.noreply.github.com>
2017-04-06 14:14:33 -0400

    Feature ratings (#3)
    * Rating component working.
    
    * Added color to component
    
    * STEVE ARE YOU HAPPY?
    
    * It helps if you save.

---
+++ src/app/shared/rating/app-rating.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './app-rating.component.html',
+  styleUrls: ['./app-rating.component.scss']
+})
+export class AppRatingComponent implements OnInit {
+  @Input('value')
+  public rating: number = 0;
+
+  public stars: string[] = [
+    'fa-star-o',
+    'fa-star-o',
+    'fa-star-o',
+    'fa-star-o',
+    'fa-star-o'
+  ];
+
+  ngOnInit() {
+    this.stars = this.stars.map((el, idx) => {
+      let value = ++idx;
+      let isNotWhole = (this.rating % 1 !== 0);
+
+      if (this.rating >= value) {
+        return 'fa-star';
+      } else if (isNotWhole && (Math.ceil(this.rating) === value)) {
+        return 'fa-star-half-full';
+      } else {
+        return el;
+      }
+    });
+  }
+}
